38e38595ed8fc0a21935b84c1997b678d8732eed
Peter Olsen <pto@me.com>
2017-06-01 19:14:00 -0400

    Adding simple complication

---
+++ DCTrains WatchKit Extension/ComplicationController.swift
@@ -0,0 +1,73 @@
+//
+//  ComplicationController.swift
+//  WatchDemo WatchKit Extension
+//
+//  Created by Peter Olsen on 5/31/17.
+//  Copyright © 2017 Peter Olsen. All rights reserved.
+//
+
+import ClockKit
+
+
+class ComplicationController: NSObject, CLKComplicationDataSource {
+    
+    // MARK: - Timeline Configuration
+    
+    func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
+        handler([])
+    }
+    
+    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
+        handler(nil)
+    }
+    
+    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
+        handler(nil)
+    }
+    
+    func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
+        handler(.showOnLockScreen)
+    }
+    
+    // MARK: - Timeline Population
+    
+    func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+        // Call the handler with the current timeline entry
+        let template = CLKComplicationTemplateCircularSmallSimpleImage()
+        let image = UIImage(named: "Complication/Circular")
+        template.imageProvider = CLKImageProvider(onePieceImage: image!)
+        let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+        handler(entry)
+//        let template = CLKComplicationTemplateCircularSmallSimpleText()
+//        template.textProvider = CLKSimpleTextProvider(text: "M")
+//        let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+//        print("getCurrentTimelineEntry()")
+//        handler(entry)
+    }
+    
+    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
+        // Call the handler with the timeline entries prior to the given date
+        handler(nil)
+    }
+    
+    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
+        // Call the handler with the timeline entries after to the given date
+        handler(nil)
+    }
+    
+    // MARK: - Placeholder Templates
+    
+    func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
+        // This method will be called once per supported complication, and the results will be cached
+        let image = UIImage(named: "Complication/Circular")
+        let imageProvider = CLKImageProvider(onePieceImage: image!)
+        let complicationTemplate = CLKComplicationTemplateCircularSmallSimpleImage()
+        complicationTemplate.imageProvider = imageProvider
+        handler(complicationTemplate)
+//        let template = CLKComplicationTemplateCircularSmallSimpleText()
+//        template.textProvider = CLKSimpleTextProvider(text: "M")
+//        print("getLocalizableSampleTemplate()")
+//        handler(template)
+    }
+    
+}
